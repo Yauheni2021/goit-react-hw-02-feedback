3175af4930fd215677b1c00238ef1815821f62fd
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-07-10 12:10:56 +0200

    add statistics feedback notification and styled

--- src/components/Feedback/FeedbackOptions.styled.js
+++ src/components/Feedback/FeedbackOptions.styled.js
@@ -2,12 +2,23 @@ import styled from "styled-components";
 
 export const Button = styled.button`
 padding: ${p => `${p.theme.space[1]}px`} 0;
-margin-rigth: ${p => `${p.theme.space[3]}px`} 0;
+margin-rigth: ${p => `${p.theme.space[3]}px`} ;
 :last-child {
     margin-right: 0;
 }
 
 text-aligin: center;
 font-size: ${p => p.theme.fontSizes.m};
-
+  color: ${p => p.theme.colors.text};
+  background-color: ${p => p.theme.colors.muted};
+  :hover {
+    background-color: ${p => p.theme.colors.accent};
+  }
+  width: 80px;
+  height: 30px;
+  border-radius: ${p => p.theme.radii.large};
+  cursor: pointer;
+  ::first-letter {
+    text-transform: capitalize;
+  }
 `
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import { App } from 'components/App';
 import './index.css';
 import '../node_modules/modern-normalize/modern-normalize.css';
 
+import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      </ThemeProvider>
   </React.StrictMode>
 );
